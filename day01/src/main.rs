3072fae8ea3ef436847820f5a4ec1c1dae80f743
Derek Hendrickx <derek@hendrickx.me>
2017-12-09 18:11:57 -0500

    Add part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -20,8 +20,29 @@ fn find_sum_match_next_digit(inputs: &[i32]) -> i32 {
     sum
 }
 
+fn find_sum_match_halfway_round(input: &[i32]) -> i32 {
+    let half = ((input.len() as f32) * 0.5) as usize;
+    let first_half = &input[0 .. half];
+    let second_half = &input[half .. input.len()];
+    let mut sum: i32 = 0;
+
+    for (index, x) in first_half.iter().enumerate() {
+        if *x == second_half[index] {
+            sum += *x;
+        }
+    }
+
+    for (index, x) in second_half.iter().enumerate() {
+        if *x == first_half[index] {
+            sum += *x;
+        }
+    }
+
+    sum
+}
+
 fn main() {
-    let mut input_file = File::open("input/input.txt").expect("Unable to open file");
+    let mut input_file = File::open("input.txt").expect("Unable to open file");
     let mut buffer = String::new();
     let mut input = Vec::new();
 
@@ -34,8 +55,79 @@ fn main() {
             input.push(value as i32);
         }
     }
-    let sum = find_sum_match_next_digit(&input);
+    let sum_part1 = find_sum_match_next_digit(&input);
+    let sum_part2 = find_sum_match_halfway_round(&input);
 
     println!("Inputs = {:?}", input);
-    println!("Sum = {}", sum);
+    println!("Sum Part 1 = {}", sum_part1);
+    println!("Sum Part 2 = {}", sum_part2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::find_sum_match_next_digit;
+    use super::find_sum_match_halfway_round;
+
+    #[test]
+    fn test_find_sum_match_next_digit_first_input() {
+        let input = [1, 1, 2, 2];
+        let sum = find_sum_match_next_digit(&input);
+        assert_eq!(sum, 3);
+    }
+
+    #[test]
+    fn test_find_sum_match_next_digit_second_input() {
+        let input = [1, 1, 1, 1];
+        let sum = find_sum_match_next_digit(&input);
+        assert_eq!(sum, 4);
+    }
+
+    #[test]
+    fn test_find_sum_match_next_digit_third_input() {
+        let input = [1, 2, 3, 4];
+        let sum = find_sum_match_next_digit(&input);
+        assert_eq!(sum, 0);
+    }
+
+    #[test]
+    fn test_find_sum_match_next_digit_fourth_input() {
+        let input = [9, 1, 2, 1, 2, 1, 2, 9];
+        let sum = find_sum_match_next_digit(&input);
+        assert_eq!(sum, 9);
+    }
+
+    #[test]
+    fn test_find_sum_match_halfway_round_first_input() {
+        let input = [1, 2, 1, 2];
+        let sum = find_sum_match_halfway_round(&input);
+        assert_eq!(sum, 6);
+    }
+
+    #[test]
+    fn test_find_sum_match_halfway_round_second_input() {
+        let input = [1, 2, 3, 4];
+        let sum = find_sum_match_halfway_round(&input);
+        assert_eq!(sum, 0);
+    }
+
+    #[test]
+    fn test_find_sum_match_halfway_round_third_input() {
+        let input = [1, 2, 3, 4, 2, 5];
+        let sum = find_sum_match_halfway_round(&input);
+        assert_eq!(sum, 4);
+    }
+
+    #[test]
+    fn test_find_sum_match_halfway_round_fourth_input() {
+        let input = [1, 2, 3, 1, 2, 3];
+        let sum = find_sum_match_halfway_round(&input);
+        assert_eq!(sum, 12);
+    }
+
+    #[test]
+    fn test_find_sum_match_halfway_round_fifth_input() {
+        let input = [1, 2, 1, 3, 1, 4, 1, 5];
+        let sum = find_sum_match_halfway_round(&input);
+        assert_eq!(sum, 4);
+    }
 }
