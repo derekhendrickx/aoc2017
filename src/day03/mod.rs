998c7676594715f2ccdf7b6c743988d942b32b11
Derek Hendrickx <derek@hendrickx.me>
2017-12-25 16:10:09 -0500

    Add tests cases

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -66,15 +66,23 @@ fn get_manhattan_distance(n: i32, input: i32) -> i32 {
             },
         }
     }
-    println!("{:?}", spiral_memory);
 
+    let flatten_spiral = spiral_memory.iter()
+    .flat_map(|row| row.iter())
+    .cloned()
+    .collect::<Vec<i32>>();
+    let origin_position = flatten_spiral.iter()
+    .position(|&value| value == 1)
+    .unwrap();
+    let origin = ((origin_position as i32) / n, (origin_position as i32) % n);
+    let input_position = flatten_spiral.iter()
+    .position(|&value| value == input)
+    .unwrap();
+    let destination = ((input_position as i32) / n, (input_position as i32) % n);
     // Manhattan distance
     // (x1 - x2) + (y1 - y2)
-    let x2 = x as i32;
-    let y2 = y as i32;
-    let distance: i32 = (x1 - x2) + (y1 - y2);
 
-    distance.abs()
+    (origin.0 - destination.0).abs() + (origin.1 - destination.1).abs()
 }
 
 #[cfg(test)]
@@ -93,5 +101,19 @@ mod tests {
         let input = 12;
         let manhattan_distance = get_manhattan_distance(5, input);
         assert_eq!(3, manhattan_distance);
+    }
+
+    #[test]
+    fn test_get_manhattan_distance_sample_input_3() {
+        let input = 23;
+        let manhattan_distance = get_manhattan_distance(5, input);
+        assert_eq!(2, manhattan_distance);
+    }
+
+    #[test]
+    fn test_get_manhattan_distance_sample_input_4() {
+        let input = 1024;
+        let manhattan_distance = get_manhattan_distance(5, input);
+        assert_eq!(31, manhattan_distance);
     }
 }
